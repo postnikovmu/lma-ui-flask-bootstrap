b25963c150b56fd39d8f868fa555308e88864172
Mikhail Postnikov <postnikovmu@mail.ru>
2022-11-05 17:33:25 +0300

    c:; cd 'c:\Work\TempPy\20220620 -PyFlaskApp\py-flask-vac-app'; & 'c:\Work\TempPy\20220620 -PyFlaskApp\py-flask-vac-app\venv\Scripts\python.exe' 'c:\Users\A736638\.vscode\extensions\ms-python.python-2022.18.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '53483' '--' '-m' 'flask' 'run' '--no-debugger' '--no-reload'

--- py-flask-vac-app.py
+++ py-flask-vac-app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, request, url_for, flash, redirect
 #from flask import jsonify
-
 import json
 import requests
 import operator
@@ -19,6 +18,7 @@ def call_hh_webapi(text,region):
     responce_json = data
     return responce_json
 
+
 def analisys(dict_of_key_skills, responce):
     for line in responce:
         strArrKeySkills = line["strArrKeySkills"]
@@ -30,13 +30,25 @@ def analisys(dict_of_key_skills, responce):
             else:
                 dict_of_key_skills[key_skill['name']] += 1   
 
+
+def call_back_serv(text,region):
+    api_url = 'https://govacbackserv.cfapps.us10.hana.ondemand.com/?text=' + text + '&' + 'area=' + region
+    res = requests.get(api_url)
+    data = json.loads(res.text)
+    responce_json = data
+    return responce_json
+
 #from contextlib import redirect_stdout
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 3000))
+
+
 @app.route('/')
 def hello():
     return "Hello from Python!"
+
+
 #@app.route('/hh1/', methods=('GET', 'POST'))
 #def hh1():
 #    str_text = ""
@@ -45,6 +57,7 @@ def hello():
 #        str_text = request.form['strText']
 #        str_area = request.form['strArea']
 #    return render_template('index.html', strText =  str_text, strArea = str_area )    
+
 
 @app.route('/hh2/', methods=('GET', 'POST'))
 def hh2():
@@ -55,7 +68,6 @@ def hh2():
     if request.method == 'POST':
         str_text = request.form['strText']
         str_area = request.form['strArea']
-
     
         text = str_text
         region = str_area
@@ -99,8 +111,9 @@ def hh4():
         return sorted_d
 
 
+# The endpoint is deprecated
 @app.route('/hh5/', methods=('GET', 'POST'))
-def hh2():
+def hh5():
     str_text = ""
     str_area = ""
     sorted_d = {}
@@ -109,18 +122,45 @@ def hh2():
         str_text = request.form['strText']
         str_area = request.form['strArea']
 
-    
         text = str_text
         region = str_area
         dict_of_key_skills = {} 
         responce = call_hh_webapi(text,region)
-        #analisys(dict_of_key_skills, responce)
-        #sorted_d = dict( sorted(dict_of_key_skills.items(), key=operator.itemgetter(1),reverse=True))
-        #items_num = str(len(sorted_d)) + " skills are found"
+        # analisys(dict_of_key_skills, responce)
+        # sorted_d = dict( sorted(dict_of_key_skills.items(), key=operator.itemgetter(1),reverse=True))
+        # items_num = str(len(sorted_d)) + " skills are found"
     return render_template('index_2.html', strText =  str_text, strArea = str_area, skill_list = sorted_d, strItemsNum = items_num )
+
+
+# The endpoint for the central backend service 'go_vac_back_serv'
+@app.route('/hh6/', methods=('GET', 'POST'))
+def hh6():
+    str_text = ""
+    str_area = ""
+    sorted_d = {}
+    items_num = ""
+
+    if request.method == 'POST':
+        str_text = request.form['strText']
+        str_area = request.form['strArea']
+        text = str_text
+        region = str_area
+        dict_of_key_skills = {}
+        
+        responce = call_back_serv(text,region)
+        point1_data = responce['Point1']
+        if point1_data['strErr'] == "":
+            items_num = str(point1_data['arrData'][0]['sSummary'][0]['intCount']) + " skills are found"
+            point1_skills = point1_data['arrData'][0]['sSummary'][1]['arrTerm']
+            for skill in point1_skills:
+                key = skill['strTerm'] 
+                value = round(skill['dblQuota'], 3)
+                sorted_d[key] = value
+        else:
+            items_num = point1_data['strErr']
+
+    return render_template('index_6.html', strText =  str_text, strArea = str_area, skill_list = sorted_d, strItemsNum = items_num )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
-    #app.run(host='localhost', port=port)
-
- 
+    # app.run(host='localhost', port=port)
